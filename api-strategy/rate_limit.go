7e5590b8280b2c60b4c2a60d70f8bba2fc352707
pefish <pefish@qq.com>
2023-07-19 23:05:35 +0800

    Update rate_limit.go

--- api-strategy/rate_limit.go
+++ api-strategy/rate_limit.go
@@ -100,6 +100,6 @@ func (rls *RateLimitStrategy) takeAvailable(out api_session.IApiSession, block b
 			takenResult = false
 		}
 	}
-	out.Logger().DebugF("current rate limit token count: %d", len(rls.tokenBucket))
+	out.Logger().DebugF("current rate limit token count: %d, takenResult: %t", len(rls.tokenBucket), takenResult)
 	return takenResult
 }
